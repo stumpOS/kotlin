d400e60ec27256ef4d32b88de4bf8f402c62a478
Anastasiya Shadrina <a.shadrina5@mail.ru>
2021-02-17 02:30:18 +0700

    [FE] Add receiver expressions to scope

--- compiler/frontend/src/org/jetbrains/kotlin/resolve/BodyResolver.java
+++ compiler/frontend/src/org/jetbrains/kotlin/resolve/BodyResolver.java
@@ -1010,6 +1010,23 @@ public class BodyResolver {
                 localContext != null ? localContext.inferenceSession : null
         );
 
+        if (function instanceof KtFunction) {
+            KtReceiverExpressionList receiverExpressionList = ((KtFunction) function).getReceiverExpressionList();
+            if (receiverExpressionList != null) {
+                ExpressionTypingContext context = ExpressionTypingContext.newContext(
+                        trace, headerScope, outerDataFlowInfo, TypeUtils.NO_EXPECTED_TYPE,
+                        languageVersionSettings, valueParameterResolver.getDataFlowValueFactory()
+                );
+                innerScope = FunctionDescriptorUtil.makeFunctionInnerScopeWithAdditionalReceiverObjects(
+                        receiverExpressionList,
+                        functionDescriptor,
+                        innerScope,
+                        context,
+                        expressionTypingServices
+                );
+            }
+        }
+
         // Synthetic "field" creation
         if (functionDescriptor instanceof PropertyAccessorDescriptor && functionDescriptor.getExtensionReceiverParameter() == null) {
             PropertyAccessorDescriptor accessorDescriptor = (PropertyAccessorDescriptor) functionDescriptor;
